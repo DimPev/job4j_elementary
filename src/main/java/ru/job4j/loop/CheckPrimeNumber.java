fbc489c849a399ad5ac80c3b2f2a81386cb00d2c
DimPev <dmitriiPev777@gmail.com>
2022-10-30 12:45:19 +0300

    5.5. Простое число [#171694]

---
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,20 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = false;
+        int stop = 0;
+        for (int index = 2; index <= number; index++) {
+            if (number % index == 0) {
+                stop += 1;
+                prime = true;
+            } else if (stop == 1) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+}
+
+
