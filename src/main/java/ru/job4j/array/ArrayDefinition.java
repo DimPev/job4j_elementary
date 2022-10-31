2415ee54c2c15d1345776c32fe01c018bd5ddc47
DimPev <dmitriiPev777@gmail.com>
2022-10-31 23:00:04 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,8 +6,17 @@ public class ArrayDefinition {
         short[] age = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Korobeinikov Stas";
+        names[1] = "Mikhail Lukonin";
+        names[2] = "Петр Арсентьев";
+        names[3] = "Kartashova Elena";
         System.out.println("Размер массива равен: " + age.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        System.out.println("Ответственный ментор: " + names[0]);
+        System.out.println("Ответственный ментор: " + names[1]);
+        System.out.println("Ответственный ментор: " + names[2]);
+        System.out.println("Ответственный ментор: " + names[3]);
     }
 }
