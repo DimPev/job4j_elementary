174032cdce96ff7ad4bceaaf8307cdff6203928d
DimPev <dmitriiPev777@gmail.com>
2022-11-01 19:14:07 +0300

    6.0.7. Массивы в тестах. [#226789]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -2,11 +2,10 @@ package ru.job4j.array;
 
 public class Square {
     public static int[] calculate(int bound) {
-
         int[] rst = new int[bound];
         for (int index = 1; index < rst.length; index++) {
-            double indexToPow = Math.pow(index, index);
-            rst[index] = (int) Math.round(indexToPow);
+
+            rst[index] = index * index;
         }
         return rst;
     }
