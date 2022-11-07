1a4665f8c751ebfeefc2b2f7a24639f5d8762757
DimPev <dmitriiPev777@gmail.com>
2022-11-07 08:55:56 +0300

    6.6.3. Двухмерный массив. If. [#306727]

--- src/test/java/ru/job4j/array/Sequence.java
+++ src/test/java/ru/job4j/array/Sequence.java
@@ -7,11 +7,10 @@ public class Sequence {
         array[0][0] = 1;
         array[0][1] = 2;
         array[0][2] = 3;
-        array[0][3] = 4;
-        array[1][0] = 5;
-        array[1][1] = 6;
-        array[1][2] = 7;
-        array[1][3] = 8;
-        array[2][0] = 9;
+        array[1][0] = 4;
+        array[1][1] = 5;
+        array[1][2] = 6;
+        array[2][1] = 8;
+        array[2][2] = 9;
     }
 }
