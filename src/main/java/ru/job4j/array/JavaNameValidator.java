c00bbd2d4f45df53f8455a808c8d55940aea7cd4
DimPev <dmitriiPev777@gmail.com>
2022-11-13 10:15:46 +0300

    6.9. String - это массив [#504884 #359723]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -1,43 +1,21 @@
 package ru.job4j.array;
 
-public class JavaNameValidator {
+import static java.lang.Character.isUpperCase;
+import static java.lang.Character.isDigit;
 
+public class JavaNameValidator {
     public static boolean isNameValid(String name) {
+        boolean rls = true;
         char[] isText = name.toCharArray();
-        for (int i = 0; i < isText.length; i++) {
-            int code = name.codePointAt(i);
-            if (isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code) || isDigit(code)) {
-                return true;
+        if (!name.isEmpty() && !isUpperCase(name.codePointAt(0))) {
+            for (int i = 1; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                return code == 96 || code == 36 || code >= 65 && code <= 90 || code >= 97 && code <= 122;
             }
         }
         return false;
     }
-
-    public static boolean isSpecialSymbol(int code) {
-        if (code == 96 || code == 36) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean isUpperLatinLetter(int code) {
-        if (code >= 65 && code <= 90) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean isLowerLatinLetter(int code) {
-        if (code >= 97 && code <= 122) {
-            return true;
-        }
-        return false;
-    }
-
-    public static boolean isDigit(int code) {
-        if (code >= 48 && code <= 57) {
-            return true;
-        }
-        return false;
-    }
 }
+
+
+
