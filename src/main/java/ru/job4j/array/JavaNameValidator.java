b72ee03fea4eccaef0464f2008bc86fb3e55aea1
DimPev <dmitriiPev777@gmail.com>
2022-11-16 12:01:57 +0300

    6.9. String - это массив [#504884 #359723]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -1,17 +1,34 @@
 package ru.job4j.array;
 
 import static java.lang.Character.isUpperCase;
-import static java.lang.Character.isDigit;
 
 public class JavaNameValidator {
+
     public static boolean isNameValid(String name) {
-        char[] isText = name.toCharArray();
-        if (!name.isEmpty() && !isUpperCase(name.codePointAt(0))) {
+        boolean rls = false;
+        if (!name.isEmpty() && !isUpperCase(name.codePointAt(0)) && !Character.isDigit(name.codePointAt(0))) {
             for (int i = 1; i < name.length(); i++) {
+                System.out.println(i);
                 int code = name.codePointAt(i);
-                return code == 96 || code == 36 || code >= 65 && code <= 90 || code >= 97 && code <= 122;
+                if (!Character.isDigit(name.codePointAt(i)) && isNotSpecialSymbol(code) && isNotUpperLatinLetter(code) && isNotLowerLatinLetter(code)) {
+                    rls = false;
+                    break;
+                }
+                rls = true;
             }
         }
-        return false;
+        return rls;
+    }
+
+    private static boolean isNotLowerLatinLetter(int code) {
+        return code < 97 || code > 122;
+    }
+
+    private static boolean isNotUpperLatinLetter(int code) {
+        return code < 65 || code > 90;
+    }
+
+    private static boolean isNotSpecialSymbol(int code) {
+        return code != 36 && code != 95;
     }
 }
